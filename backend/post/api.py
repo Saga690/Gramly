f65b4c0e1a127273687fa3fcbe94ede751b5c6f3
Saga690 <master.ayush.2003@gmail.com>
2024-06-12 19:56:09 +0530

    made it possible to add text posts

--- backend/post/api.py
+++ backend/post/api.py
@@ -5,6 +5,8 @@ from .serializers import PostSerializer
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
+from .forms import PostForm
+
 
 @api_view(['GET'])
 def post_list(request):
@@ -13,3 +15,22 @@ def post_list(request):
     serializer = PostSerializer(posts, many= True)
 
     return JsonResponse(serializer.data, safe = False)
+
+
+@api_view(['POST'])
+def post_create(request):
+
+    form = PostForm(request.data)
+
+    if form.is_valid():
+        post = form.save(commit = False)
+        post.created_by = request.user
+        post.save()
+
+        serializer = PostSerializer(post)
+
+        return JsonResponse(serializer.data, safe = False)
+    
+    else:
+
+        return JsonResponse({'error': 'error'})
