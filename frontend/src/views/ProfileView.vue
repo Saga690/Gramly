2e1c522cf470a4857b84d3fcb933b821cc49760a
Saga690 <master.ayush.2003@gmail.com>
2024-06-14 00:31:38 +0530

    increase posts count when creating posts

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -196,6 +196,7 @@ export default {
 
                     this.posts.unshift(response.data)
                     this.body = ''
+                    this.user.posts_count += 1
                 })
                 .catch(error => {
                     console.log('error', error)
