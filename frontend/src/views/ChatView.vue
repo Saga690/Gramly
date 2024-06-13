358c54298440492c46d93d38128ba5e4a1eda24a
Saga690 <master.ayush.2003@gmail.com>
2024-06-13 18:16:22 +0530

    made it possible to send messages

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -4,7 +4,7 @@
             <div class="p-4 bg-white border border-gray-200 rounded-lg">
                 <div class="space-y-4">
                     <div class="flex items-center justify-between" v-for="conversation in conversations"
-                        v-bind:key="conversation.id">
+                        v-bind:key="conversation.id" v-on:click="setActiveConversation(conversation.id)">
                         <div class="flex items-center space-x-2">
                             <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
 
@@ -22,55 +22,55 @@
         <div class="main-center col-span-3 space-y-4">
             <div class="bg-white border border-gray-200 rounded-lg">
                 <div class="flex flex-col flex-grow p-4">
-                    <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
-                        <div>
-                            <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod.</p>
+                    <template v-for="message in activeConversation.messages" v-bind:key="message.id">
+                        <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end"
+                            v-if="message.created_by.id == userStore.user.id">
+                            <div>
+                                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                                    <p class="text-sm">
+                                        {{ message.body }}
+                                    </p>
+                                </div>
+                                <span class="text-xs text-gray-500 leading-none">{{ message.created_at_formatted }}
+                                    ago</span>
                             </div>
-                            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+                            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+                                <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
+                            </div>
                         </div>
-                        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                        </div>
-                    </div>
 
-                    <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
-                        <div>
-                            <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                                <p class="text-sm">Lorem ipsum dolor sit amet</p>
-                            </div>
-                            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-                        </div>
-                        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                        </div>
-                    </div>
 
-                    <div class="flex w-full mt-2 space-x-3 max-w-md">
-                        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                        </div>
-                        <div>
-                            <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-                                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <div class="flex w-full mt-2 space-x-3 max-w-md" v-else>
+                            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+                                <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
                             </div>
-                            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+                            <div>
+                                <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                                    <p class="text-sm">{{ message.body }} </p>
+                                </div>
+                                <span class="text-xs text-gray-500 leading-none">{{ message.created_at_formatted }}
+                                    ago</span>
+                            </div>
                         </div>
-                    </div>
+                    </template>
+
+
+
+
                 </div>
             </div>
 
             <div class="bg-white border border-gray-200 rounded-lg">
-                <div class="p-4">
-                    <textarea class="p-4 w-full bg-gray-100 rounded-lg"
-                        placeholder="What do you want to say?"></textarea>
-                </div>
+                <form v-on:submit.prevent="submitForm">
+                    <div class="p-4">
+                        <textarea v-model="body" class="p-4 w-full bg-gray-100 rounded-lg"
+                            placeholder="What do you want to say?"></textarea>
+                    </div>
 
-                <div class="p-4 border-t border-gray-100 flex justify-between">
-                    <a href="#" class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Post</a>
-                </div>
+                    <div class="p-4 border-t border-gray-100 flex justify-between">
+                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Send</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -92,11 +92,9 @@ export default {
 
     data() {
         return {
-            conversations: [
-                {
-                    users: []
-                }
-            ]
+            conversations: [],
+            activeConversation: {},
+            body: ''
         }
     },
 
@@ -105,6 +103,13 @@ export default {
     },
 
     methods: {
+
+        setActiveConversation(id) {
+            console.log('setActiveConversation', id)
+            this.activeConversation = id
+            this.getMessages()
+        },
+
         getConversations() {
             console.log('getConv')
 
@@ -112,11 +117,44 @@ export default {
                 .get('/api/chat/')
                 .then(response => {
                     console.log(response.data)
+
                     this.conversations = response.data
-                    // if (this.conversations.length) {
-                    //     this.activeConversation = this.conversations[0].id
-                    // }
-                    // this.getMessages()
+
+                    if (this.conversations.length) {
+                        this.activeConversation = this.conversations[0].id
+                    }
+                    this.getMessages()
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+
+        getMessages() {
+            console.log('getMessages')
+            axios
+                .get(`/api/chat/${this.activeConversation}/`)
+
+                .then(response => {
+                    console.log(response.data)
+
+                    this.activeConversation = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+
+        submitForm() {
+            console.log('submitForm', this.body)
+            axios
+                .post(`/api/chat/${this.activeConversation.id}/send/`, {
+                    body: this.body
+                })
+                .then(response => {
+                    console.log(response.data)
+
+                    this.activeConversation.messages.push(response.data)
                 })
                 .catch(error => {
                     console.log(error)
