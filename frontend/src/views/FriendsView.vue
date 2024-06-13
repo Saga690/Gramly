6ff634e16dec58722a04c10a32f9123cc1b0d21b
Saga690 <master.ayush.2003@gmail.com>
2024-06-13 19:14:16 +0530

    show number of posts on profile

--- frontend/src/views/FriendsView.vue
+++ frontend/src/views/FriendsView.vue
@@ -8,65 +8,55 @@
 
                 <div class="mt-6 flex space-x-8 justify-around">
                     <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
-                    <p class="text-xs text-gray-500">120 posts</p>
+                    <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
                 </div>
             </div>
         </div>
 
         <div class="main-center col-span-2 space-y-4">
-            <div 
-                class="p-4 bg-white border border-gray-200 rounded-lg"
-                v-if="friendshipRequests.length"
-            >
+            <div class="p-4 bg-white border border-gray-200 rounded-lg" v-if="friendshipRequests.length">
                 <h2 class="mb-6 text-xl">Friendship requests</h2>
 
-                <div 
-                    class="p-4 text-center bg-gray-100 rounded-lg"
-                    v-for="friendshipRequest in friendshipRequests"
-                    v-bind:key="friendshipRequest.id"
-                >
+                <div class="p-4 text-center bg-gray-100 rounded-lg" v-for="friendshipRequest in friendshipRequests"
+                    v-bind:key="friendshipRequest.id">
                     <img src="https://i.pravatar.cc/100?img=70" class="mb-6 mx-auto rounded-full">
 
                     <p>
                         <strong>
-                            <RouterLink :to="{name: 'profile', params:{'id': friendshipRequest.created_by.id}}">{{ friendshipRequest.created_by.name }}</RouterLink>
+                            <RouterLink :to="{ name: 'profile', params: { 'id': friendshipRequest.created_by.id } }">{{
+                                friendshipRequest.created_by.name }}</RouterLink>
                         </strong>
                     </p>
 
                     <div class="mt-6 flex space-x-8 justify-around">
                         <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
-                        <p class="text-xs text-gray-500">120 posts</p>
+                        <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
                     </div>
 
                     <div class="mt-6 space-x-4">
-                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg" @click="handleRequest('accepted', friendshipRequest.created_by.id)">Accept</button>
-                        <button class="inline-block py-4 px-6 bg-red-600 text-white rounded-lg" @click="handleRequest('rejected', friendshipRequest.created_by.id)">Reject</button>
+                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg"
+                            @click="handleRequest('accepted', friendshipRequest.created_by.id)">Accept</button>
+                        <button class="inline-block py-4 px-6 bg-red-600 text-white rounded-lg"
+                            @click="handleRequest('rejected', friendshipRequest.created_by.id)">Reject</button>
                     </div>
                 </div>
 
                 <hr>
             </div>
 
-            <div 
-                class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-2 gap-4"
-                v-if="friends.length"
-            >
-                <div 
-                    class="p-4 text-center bg-gray-100 rounded-lg"
-                    v-for="user in friends"
-                    v-bind:key="user.id"
-                >
+            <div class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-2 gap-4" v-if="friends.length">
+                <div class="p-4 text-center bg-gray-100 rounded-lg" v-for="user in friends" v-bind:key="user.id">
                     <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full">
 
                     <p>
                         <strong>
-                            <RouterLink :to="{name: 'profile', params:{'id': user.id}}">{{ user.name }}</RouterLink>
+                            <RouterLink :to="{ name: 'profile', params: { 'id': user.id } }">{{ user.name }}</RouterLink>
                         </strong>
                     </p>
 
                     <div class="mt-6 flex space-x-8 justify-around">
                         <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
-                        <p class="text-xs text-gray-500">120 posts</p>
+                        <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
                     </div>
                 </div>
             </div>
