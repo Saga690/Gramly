7f197192ff4bc727e381dfe04a8187c098afb44c
Saga690 <master.ayush.2003@gmail.com>
2024-06-14 11:09:25 +0530

    friendship request minor fix

--- backend/post/api.py
+++ backend/post/api.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from notification.utils import create_notification
 
 from .forms import PostForm, AttachmentForm
-from account.models import User
+from account.models import User, FriendshipRequest
 from account.serializers import UserSerializer
 
 
@@ -50,9 +50,21 @@ def post_list_profile(request, id):
     posts_serializer = PostSerializer(posts, many= True)
     user_serializer = UserSerializer(user)
 
+    can_send_friendship_request = True
+
+    if request.user in user.friends.all():
+        can_send_friendship_request = False
+
+    check1 = FriendshipRequest.objects.filter(created_for=request.user).filter(created_by=user)
+    check2 = FriendshipRequest.objects.filter(created_for=user).filter(created_by=request.user)
+
+    if check1 or check2:
+        can_send_friendship_request = False
+
     return JsonResponse({
         'posts': posts_serializer.data,
-        'user': user_serializer.data
+        'user': user_serializer.data,
+        'can_send_friendship_request': can_send_friendship_request
     }, safe=False)
 
 
