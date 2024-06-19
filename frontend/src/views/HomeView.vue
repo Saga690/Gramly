67a483fb190ca587c2e2d308f5847ece1e3ea6e8
Saga690 <master.ayush.2003@gmail.com>
2024-06-19 21:11:39 +0530

    added homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,10 @@
 </script>
 
 <template>
-  <main>
-    <h1>Welcome</h1>
+  <main class="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+    <h1 class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 mb-4">
+      Welcome to <span>GRAMLY</span>
+    </h1>
+    <p class="text-lg text-gray-600">Please login or signup to continue</p>
   </main>
 </template>
